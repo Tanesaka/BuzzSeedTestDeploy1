7b80a589561e00ee15693bac4b5944d5bdd5bfa4
Tanesaka <seedtanesa@outlook.jp>
2021-01-03 15:41:04 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   scope module: :public do
     resources :users, only:[:edit, :update, :show, :index]
     resources :themes, only:[:index, :create, :new, :show, :destroy]
+    get 'answers/rankindex' => 'answers#rankindex'
     resources :answers, only:[:index, :create, :new, :show, :destroy] do
       resource :favorites, only: [:create, :destroy]
       resources :comments, only: [:create, :destroy]
