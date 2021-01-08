ca41ec403c24ba9b7d79e6f71c62d9c7bac713c4
Tanesaka <seedtanesa@outlook.jp>
2021-01-08 08:32:10 +0000

    Update

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,18 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    answer = Answer.find(params[:answer_id])
-    favorite = current_user.favorites.new(answer_id: answer.id)
+    @answer = Answer.find(params[:answer_id])
+    favorite = current_user.favorites.new(answer_id: @answer.id)
     favorite.save
-    redirect_to request.referer
+    # いいね非同期化のためリダイレクトしない
+    # redirect_to request.referer
   end
 
   def destroy
-    answer = Answer.find(params[:answer_id])
-    favorite = current_user.favorites.find_by(answer_id: answer.id)
+    @answer = Answer.find(params[:answer_id])
+    favorite = current_user.favorites.find_by(answer_id: @answer.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   # private
